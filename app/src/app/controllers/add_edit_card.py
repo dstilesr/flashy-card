5c5b0a6ce672803e1764273640780d0e21f10ac8
dstilesr <d.stiles.r@hotmail.com>
2023-12-17 14:56:33 -0500

    Handlers to add / edit cards

---
+++ app/src/app/controllers/add_edit_card.py
@@ -0,0 +1,79 @@
+import jinja2
+import sqlalchemy as sa
+from typing import Optional
+from app import models as m
+from datetime import datetime
+import sqlalchemy.ext.asyncio as sa_async
+from fastapi.responses import RedirectResponse
+
+from .base import BaseController
+from ..enums import PartOfSpeech
+
+
+class EditCard(BaseController):
+    """
+    Controller to Edit or Create flash cards.
+    """
+
+    def __init__(
+            self,
+            engine: sa_async.AsyncEngine,
+            word: str,
+            translation: str,
+            language_id: int,
+            part_of_speech: PartOfSpeech,
+            card_id: Optional[int] = None,
+            sentence: Optional[str] = None,
+            template_env: Optional[jinja2.Environment] = None):
+        """
+        :param engine:
+        :param word:
+        :param translation:
+        :param language_id:
+        :param part_of_speech:
+        :param card_id:
+        :param sentence:
+        :param template_env:
+        """
+        super().__init__(engine, template_env)
+        self.is_create = card_id is None
+        self.card_id = card_id
+        self.args = {
+            "target_word": word,
+            "translation": translation,
+            "target_language_id": language_id,
+            "sentence": sentence,
+            "part_of_speech": part_of_speech
+        }
+        if not self.is_create:
+            self.args.update(
+                id=card_id,
+                updated_at=datetime.utcnow()
+            )
+
+    async def process_request(self) -> RedirectResponse:
+        """
+        Process request to add or edit card.
+        :return:
+        """
+        async with sa_async.AsyncSession(self.engine) as s:
+            if self.is_create:
+                new_card = m.FlashCard(**self.args)
+                s.add(new_card)
+            else:
+                await s.execute(
+                    sa.update(m.FlashCard)
+                    .where(m.FlashCard.id == self.card_id)
+                    .values(**self.args)
+                )
+            await s.commit()
+            res = await s.scalars(
+                s.select(m.Language)
+                .where(m.Language.id == self.args["language_id"])
+            )
+            lang = res.one()
+
+        return RedirectResponse(
+            f"/cards/{lang.slug}/list",
+            status_code=302
+        )
--- app/src/app/routes/cards.py
+++ app/src/app/routes/cards.py
@@ -1,8 +1,10 @@
-from typing import Annotated
 from fastapi import APIRouter, Form
+from typing import Annotated, Optional
 from fastapi.responses import HTMLResponse, RedirectResponse
 
 from ..db import DB_ENGINE
+from ..enums import PartOfSpeech
+from ..controllers.add_edit_card import EditCard
 from ..controllers.list_cards import ListCardsLang
 
 card_router = APIRouter()
@@ -17,4 +19,63 @@ async def list_cards_language(language_slug: str) -> HTMLResponse:
     """
     controller = ListCardsLang(DB_ENGINE, language_slug=language_slug)
     rsp = await controller.process_request()
+    return rsp
+
+
+@card_router.post("/add")
+async def add_card(
+        word: Annotated[str, Form()],
+        translation: Annotated[str, Form()],
+        language_id: Annotated[int, Form()],
+        part_of_speech: Annotated[PartOfSpeech, Form()],
+        sentence: Annotated[Optional[str], Form()] = None) -> RedirectResponse:
+    """
+    Add a new flash card.
+    :param word:
+    :param translation:
+    :param language_id:
+    :param part_of_speech:
+    :param sentence:
+    :return:
+    """
+    handler = EditCard(
+        DB_ENGINE,
+        word=word,
+        translation=translation,
+        language_id=language_id,
+        part_of_speech=part_of_speech,
+        sentence=sentence if sentence != "" else None
+    )
+    rsp = await handler.process_request()
+    return rsp
+
+
+@card_router.post("/{card_id}/edit")
+async def edit_card(
+        card_id: int,
+        word: Annotated[str, Form()],
+        translation: Annotated[str, Form()],
+        language_id: Annotated[int, Form()],
+        part_of_speech: Annotated[PartOfSpeech, Form()],
+        sentence: Annotated[Optional[str], Form()] = None) -> RedirectResponse:
+    """
+    Add a new flash card.
+    :param word:
+    :param translation:
+    :param language_id:
+    :param part_of_speech:
+    :param card_id:
+    :param sentence:
+    :return:
+    """
+    handler = EditCard(
+        DB_ENGINE,
+        word=word,
+        translation=translation,
+        language_id=language_id,
+        part_of_speech=part_of_speech,
+        sentence=sentence if sentence != "" else None,
+        card_id=card_id
+    )
+    rsp = await handler.process_request()
     return rsp
