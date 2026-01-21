aaea981764510c7878ab0154bf5141f930a00445
dstilesr <d.stiles.r@hotmail.com>
2026-01-21 17:03:33 -0500

    Remove Python code

--- app/src/app/models/base.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.ext.asyncio import AsyncAttrs
-
-
-class Base(AsyncAttrs, DeclarativeBase):
-    """
-    Declarative base for ORM.
-    """
-    pass
